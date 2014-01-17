9b4015f363ca1161501ad93f7c3c5d93aff9a04a
Stephen Hansen <steve@hansencomputers.net>
2014-01-17 16:21:46 -0500

    Adding default css images, modifying bootstrap.php to use the local APIs autoloader instead of the global one.  Also, changing links.php to make it work (finally)

--- doc_root/bootstrap.php
+++ doc_root/bootstrap.php
@@ -1,4 +1,4 @@
 <?
-require_once("autoload.php");
+require_once(dirname(__DIR__)."/files/APIs/autoload.php");
 use \HCWeb\DB;
 //DB::Init('localhost',DB_USER,DB_PASSWD,DB_NAME);
--- doc_root/links.php
+++ doc_root/links.php
@@ -3,13 +3,13 @@ use \HCWeb\Linkbar\Bar;
 use \HCWeb\Linkbar\Link;
 
 $lbar = new Bar();
-$lbar -> addLink("Home","/");
-$lbar -> addLink("Services","/services/");
-$lbar -> addLink("Store","/store/",array(
+$lbar -> addLink(new Link("Home","/"));
+$lbar -> addLink(new Link("Services","/services/"));
+$lbar -> addLink(new Link("Store","/store/",array(
 	array("Paintings","/store/Paintings/"),
 	array("Prints","/store/Prints/"),
 	array("Services","/store/Services/"),
-));
-$lbar -> addLink("About","/about/");
+)));
+$lbar -> addLink(new Link("About","/about/"));
 
 echo "<div class=\"linkbar-wrapper\">".$lbar -> getHTML()."</div>";
